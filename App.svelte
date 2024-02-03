c5fddc82fb0ee33833a493a48458d31dc90e3083
marcoodevil <krzysaaa7@gmail.com>
2024-02-03 12:11:35 +0100

    move to svelte

---
+++ App.svelte
@@ -0,0 +1,24 @@
+<script>
+    let name = 'World';
+</script>
+
+<main>
+    <h1>Welcome!</h1>
+    <p>Hello, {name}!</p>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1rem;
+    }
+
+    h1 {
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        font-size: 1.2rem;
+    }
+</style>
