107454b6385ff80c5e650b40cd02db2c8252ac44
marcoodevil <krzysaaa7@gmail.com>
2025-02-23 12:11:01 +0100

    username change

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,27 @@
   import Game from './lib/Game.svelte';
   import { fly, fade } from 'svelte/transition'
 
-  let jamsVisible = false;
-  let prototypesVisible = false;
+  let jamsVisible = true;
+  let prototypesVisible = true;
 
   let jams = [
     {
       title: 'Melody of the Wild - LD54',
       src: 'images/panda-banjo-cropped.jpg',
       description: 'Panda, banjo, campfire.',
-      href: 'https://marcodeevil.itch.io/melody-of-the-wild'
+      href: 'https://marcodeevi.itch.io/melody-of-the-wild'
     },
     {
       title: 'Word Exorcist - LD55',
       src: 'images/word-exorcist-cropped.png',
       description: 'Not so cute typing game.',
-      href: 'https://marcodeevil.itch.io/word-exorcist'
+      href: 'https://marcodeevi.itch.io/word-exorcist'
     },
     {
       title: 'Do Not Open the Letter - LD53',
       src: 'images/do-not-open-the-letter-cropped.png',
       description: 'Do not open the letter.',
-      href: 'https://marcodeevil.itch.io/do-not-open-the-letter'
+      href: 'https://marcodeevi.itch.io/do-not-open-the-letter'
     },
   ];
 
@@ -31,19 +31,19 @@
       title: 'Subject',
       src: 'images/subject-cropped.png',
       description: 'Short, logical',
-      href: 'https://marcodeevil.itch.io/subject'
+      href: 'https://marcodeevi.itch.io/subject'
     },
     {
       title: 'Broken Dreams',
       src: 'images/broken-dreams-cropped.png',
       description: 'For now just an artistic concept...',
-      href: 'https://marcodeevil.itch.io/broken-dreams'
+      href: 'https://marcodeevi.itch.io/broken-dreams'
     },
     {
       title: 'Hard Game',
       src: 'images/hard-cropped.png',
       description: 'Just a hard game',
-      href: 'https://marcodeevil.itch.io/hard-game'
+      href: 'https://marcodeevi.itch.io/hard-game'
     },
   ];
 
@@ -58,7 +58,7 @@
       bigger projects probably here: <a href="https://whitespacestudio.dev/" target="_blank">white space studio</a>
     </p>
     <socials>
-      <a href="https://marcodeevil.itch.io/" target="_blank">
+      <a href="https://marcodeevi.itch.io/" target="_blank">
         <img src="https://static.itch.io/images/itchio-textless-black.svg" alt="Twitter" />
       </a>
       <a href="https://github.com/marcodeevi" target="_blank">
